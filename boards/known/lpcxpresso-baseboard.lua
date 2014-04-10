a3229b6baed71200330d2b8953b8c049ee6b15d7
bshurn <bshurn@gmail.com>
2014-04-10 02:57:01 -0700

    Enabling support for LPCXPresso Base Board
    Added new CPU, LPC1769
    Added new board configuration

---
+++ boards/known/lpcxpresso-baseboard.lua
@@ -0,0 +1,37 @@
+-- LPCXPresso BaseBoard build configuration
+
+return {
+  cpu = 'lpc1769',
+  components = {
+    romfs = true,
+	wofs = false,
+	shell = true,
+	advanced_shell = false,
+	sercon = { uart = 3, speed = 115200, timer=systimer, flow=none, buf_size=32 },        
+    xmodem = true,
+	term = { lines = 25, cols = 50 },
+	cints = false,
+	luaints = false,
+	tcip = false,
+	dns = false,
+	dhcp = false,
+	tcpipcon = false,
+    linenoise = { shell_lines = 10, lua_lines = 50 },
+	rfc = false,
+	mmcfs = false,
+    rpc = false,
+	sermux = false,
+    adc = false,
+	lpc17xx_semifs = false
+  },
+  config = {
+	vtmr = false,
+    egc = { mode = "disable" },
+    ram = { internal_rams = 1 }
+  },
+  modules = {
+    generic = { 'all_lua', 'cpu', 'elua', 'pd', 'pio', 'term', 'tmr', 'uart'},
+    platform = 'all'
+  }
+}
+
